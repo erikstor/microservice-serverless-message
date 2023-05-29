9896fd7e252a45283c133ae5887b5b454dff77c9
Erik Stor <erik.stor.franco@gmail.com>
2023-05-29 11:01:07 -0500

    mini-documentacion

--- aws-node-express-api-project/index.js
+++ aws-node-express-api-project/index.js
@@ -3,12 +3,23 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
-const accountSid = process.env.ACCOUNT_SID
-const authToken = process.env.AUTH_TOKEN
-const numberFromSendMessage = process.env.NUMBER_FROM_SEND_MESSAGE
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const numberFromSendMessage = process.env.NUMBER_FROM_SEND_MESSAGE;
 const client = require("twilio")(accountSid, authToken);
 
 app.use(bodyParser.json({ strict: false }));
+
+/**
+ * 
+  curl --location 'localhost:3000/send-message' \
+  --header 'Content-Type: application/json' \
+  --data '{
+      "body": "vamooooooooooooh",
+      "to": "+573153226435"
+  }'
+ * 
+ */
 
 app.post("/send-message", async (req, res, next) => {
   try {
@@ -24,9 +35,8 @@ app.post("/send-message", async (req, res, next) => {
       message: "Se envió el mensaje correctamente",
     });
   } catch (e) {
-    next(e)
+    next(e);
   }
 });
-
 
 module.exports.handler = serverless(app);
